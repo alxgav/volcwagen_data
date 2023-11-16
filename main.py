11051b7071e077840c9e89a7193270441c87284b
alxgav <alxgav@gmail.com>
2023-11-16 20:04:30 +0100

    main

--- main.py
+++ main.py
@@ -36,38 +36,48 @@ def parser(data: list):
     data_volk = []
     id = 1
     for url in data:
-        print(url)
+        print(url, id)
         html = load_data(url)
         # contact_text = html.css_first("div.SP-Contact__locality__text").text()
         title = html.css_first(
             ".SP-Contact__locality__text > p:nth-child(1) > strong:nth-child(1)"
         ).text(strip=True)
-        surname = html.css_first(
-            ".SP-Contact__locality__text > p:nth-child(2) > strong:nth-child(1)"
-        ).text(strip=True)
+        try:
+            surname = html.css_first(
+                ".SP-Contact__locality__text > p:nth-child(2) > strong:nth-child(1)"
+            ).text(strip=True)
+        except:
+            surname = ""
         address = html.css_first(".SP-Contact__locality__text > p:nth-child(3)").text(
             strip=True
         )
-        address2 = (
-            html.css_first(".SP-Contact__locality__text > p:nth-child(2)")
-            .text(strip=True)
-            .split(surname)[1]
-        )
+        if len(surname) > 0:
+            address2 = (
+                html.css_first(".SP-Contact__locality__text > p:nth-child(2)")
+                .text(strip=True)
+                .split(surname)[1]
+            )
         telefon = html.css_first(
             "ul.SP-Contact__locality__links > li:nth-child(1) > a:nth-child(1) > span:nth-child(3) > span:nth-child(1)"
         ).text()
-        email = (
-            html.css_first(
-                "li.SP-Contact__link--fullWidth:nth-child(2) > a:nth-child(1)"
+        try:
+            email = (
+                html.css_first(
+                    "li.SP-Contact__link--fullWidth:nth-child(2) > a:nth-child(1)"
+                )
+                .attributes["href"]
+                .split(":")[1]
+                .replace("%E2%9A%B9", "@")
+                .replace("%E2%97%A6", ".")
             )
-            .attributes["href"]
-            .split(":")[1]
-            .replace("%E2%9A%B9", "@")
-            .replace("%E2%97%A6", ".")
-        )
-        url = html.css_first(
-            "li.SP-Contact__link--fullWidth:nth-child(3) > a:nth-child(1)"
-        ).attributes["href"]
+        except:
+            email = ""
+        try:
+            web = html.css_first(
+                "li.SP-Contact__link--fullWidth:nth-child(3) > a:nth-child(1)"
+            ).attributes["href"]
+        except:
+            web = ""
         google_map = html.css_first(
             "div.SP-LinkList > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)"
         ).attributes["href"]
@@ -80,7 +90,7 @@ def parser(data: list):
             "address": f"{address2} {address}",
             "telefon": telefon,
             "email": email,
-            "url": url,
+            "url": web,
             "google_map": google_map,
             "open_street_map": open_street_map,
         }
@@ -88,8 +98,8 @@ def parser(data: list):
 
         data_volk.append(data)
         id += 1
-        if id == 5:
-            break
+        # if id == 5:
+        #     break
     return data_volk
 
 
